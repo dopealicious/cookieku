0888120a7788e4cd4f1f62bf001ee6d6ae8ae512
Mischa Khalqin <mischa.workspace@gmail.com>
2023-05-17 14:37:26 +0800

    membuat syarat supaya bisa login/sign-in

--- login.js
+++ login.js
@@ -1,15 +1,31 @@
 const signUpButton = document.getElementById('signUp');
-const logInButton = document.getElementById('logIn');
+const signInButton = document.getElementById('signIn');
 const container = document.getElementById("container");
+const logInButton = document.getElementById("login");
+
+let userEmail = '';
+let userPassword = '';
+
+var email = document.getElementById("email-logIn");
+email.addEventListener("input", (e) => {
+  userEmail = e.target.value;
+})
+
+var password = document.getElementById("password-logIn");
+password.addEventListener("input", (e) => {
+  userPassword = e.target.value;
+})
+
+// var password = document.getElementById("password").value;
 
 function login() {
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  if (email == '' || password == '') {
-    console.log("Hello world!");
-    alert('Login success!');
+  if (userEmail == '') {
+    alert('input email first!');
+  } else if (userPassword == ''){
+    alert('input password!');
   } else {
-    alert('Please enter the correct information!');
+    // compare database with user credential logic is here
+    alert('login success!')
   }
 }
 
@@ -26,10 +42,14 @@ function sign() {
   }
 }
 
+logInButton.addEventListener("click", () => {
+  login();
+}); 
+
 signUpButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
 
-logInButton.addEventListener("click", () => {
+signInButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
 });
