760571479346a2b15f414d2fd7196a3cccdeb8f5
Novitaazahraa <novitaazahra404@gmail.com>
2023-05-18 13:42:04 +0800

    memperbaiki menu review

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ document.querySelector("#close-form").onclick = () => {
   cart.classList.remove("active");
 };
 
-var swiper = new Swiper(".blogs-row", {
+var swiper = new Swiper(".blogs", {
   grabCursor: true,
   loop: true,
   cnteredSlides: true,
@@ -26,25 +26,39 @@ var swiper = new Swiper(".blogs-row", {
   },
 });
 
-let previewMenu = document.querySelector(".menu-preview");
-let previewBox = previewMenu.querySelectorAll(".preview");
+// Fungsi untuk membuka popup
+function openPopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.visibility = "visible";
+  popup.style.opacity = "1";
+}
 
-document.querySelectorAll(".btn").forEach((product) => {
-  product.onclick = () => {
-    previewMenu.style.display = "flex";
-    let name = product.getAttribute("data-name");
-    previewBox.forEach((preview) => {
-      let target = preview.getAttribute("data-target");
-      if (name == target) {
-        preview.classList.add("active");
-      }
-    });
-  };
-});
+// Fungsi untuk menutup popup
+function closePopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.visibility = "hidden";
+  popup.style.opacity = "0";
+}
 
-previewBox.forEach((close) => {
-  close.querySelector(".fa-xmark").onclick = () => {
-    close.classList.remove("active");
-    previewMenu.style.display = "none";
-  };
-});
+// let previewMenu = document.querySelector(".menu-preview");
+// let previewBox = previewMenu.querySelectorAll(".preview");
+
+// document.querySelectorAll(".btn").forEach((product) => {
+//   product.onclick = () => {
+//     previewMenu.style.display = "flex";
+//     let name = product.getAttribute("data-name");
+//     previewBox.forEach((preview) => {
+//       let target = preview.getAttribute("data-target");
+//       if (name == target) {
+//         preview.classList.add("active");
+//       }
+//     });
+//   };
+// });
+
+// previewBox.forEach((close) => {
+//   close.querySelector(".fa-xmark").onclick = () => {
+//     close.classList.remove("active");
+//     previewMenu.style.display = "none";
+//   };
+// });
