4c849d76d4aeac39e17ae5a854e4f1430305b687
Novitaazahraa <novitaazahra404@gmail.com>
2023-05-22 21:27:58 +0800

    menambahkan fitur ordering

--- index.js
+++ index.js
@@ -40,25 +40,79 @@ function closePopup(popupId) {
   popup.style.opacity = "0";
 }
 
-// let previewMenu = document.querySelector(".menu-preview");
-// let previewBox = previewMenu.querySelectorAll(".preview");
+function togglePaymentFields() {
+  var paymentMethod = document.getElementById("paymentMethod").value;
+  var paymentFields = document.getElementsByClassName("paymentFields");
 
-// document.querySelectorAll(".btn").forEach((product) => {
-//   product.onclick = () => {
-//     previewMenu.style.display = "flex";
-//     let name = product.getAttribute("data-name");
-//     previewBox.forEach((preview) => {
-//       let target = preview.getAttribute("data-target");
-//       if (name == target) {
-//         preview.classList.add("active");
-//       }
-//     });
-//   };
-// });
+  // Semua field pembayaran disembunyikan
+  for (var i = 0; i < paymentFields.length; i++) {
+    paymentFields[i].style.display = "none";
+  }
 
-// previewBox.forEach((close) => {
-//   close.querySelector(".fa-xmark").onclick = () => {
-//     close.classList.remove("active");
-//     previewMenu.style.display = "none";
-//   };
-// });
+  // Field pembayaran yang sesuai dengan metode dipilih ditampilkan
+  if (paymentMethod === "BankTransfer") {
+    document.getElementById("bankTransferFields").style.display = "block";
+  } else if (paymentMethod === "Ewallet") {
+    document.getElementById("ewalletFields").style.display = "block";
+  }
+}
+
+function submitForm(event) {
+  event.preventDefault(); // Menghentikan submit form
+
+  // Mengambil nilai-nilai input dari form
+  var name = document.getElementById("name").value;
+  var alamat = document.getElementById("alamat").value;
+  var catatan = document.getElementById("catatan").value;
+  var flavor = document.getElementById("flavor").value;
+  var deliveryDate = document.getElementById("deliveryDate").value;
+  var paymentMethod = document.getElementById("paymentMethod").value;
+
+  // Mengirimkan data ke server atau melakukan tindakan lainnya
+  // Contoh: Memproses pesanan dan pembayaran
+
+  // Menampilkan pesan konfirmasi
+  var message = "Terima kasih, " + name + "! Pesanan Anda telah diterima.";
+
+  if (paymentMethod === "BankTransfer") {
+    var bankName = document.getElementById("bankName").value;
+    var accountNumber = document.getElementById("accountNumber").value;
+
+    // Proses pembayaran dengan transfer bank
+
+    message +=
+      "\nMetode Pembayaran: Transfer Bank\nBank: " +
+      bankName +
+      "\nNomor Rekening: " +
+      accountNumber;
+  } else if (paymentMethod === "Ewallet") {
+    var ewalletName = document.getElementById("ewalletName").value;
+    var ewalletId = document.getElementById("ewalletId").value;
+
+    // Proses pembayaran dengan dompet elektronik
+
+    message +=
+      "\nMetode Pembayaran: Dompet Elektronik\nE-wallet: " +
+      ewalletName +
+      "\nID E-wallet: " +
+      ewalletId;
+  }
+
+  var orderSummary = document.getElementById("orderSummary");
+  orderSummary.innerHTML = `
+  <div class="card">
+    <h2>Rincian Pesanan</h2>
+    <p><strong>Nama:</strong> ${name}</p>
+    <p><strong>Alamat:</strong> ${alamat}</p>
+    <p><strong>Catatan:</strong> ${catatan}</p>
+    <p><strong>Rasa:</strong> ${flavor}</p>
+    <p><strong>Tanggal Pengambilan:</strong> ${deliveryDate}</p>
+    <p><strong>Metode Pembayaran:</strong> ${paymentMethod}</p>
+    <p><strong>Total Pembayaran:</strong> Rp.154.000</p>
+  </div>
+`;
+
+  alert(message);
+  // Reset form
+  document.getElementById("orderForm").reset();
+}
