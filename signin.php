7f0123cc348cbb2a319567125a13b00ce1bc2af5
Mischa Khalqin <mischa.workspace@gmail.com>
2023-05-24 11:41:47 +0800

    tambah login credential

--- signin.php
+++ signin.php
@@ -1,10 +1,12 @@
 <?php
 if (isset($_GET['Input'])) {
-$email = $_GET['email'];
-$password = $_GET['password'];
-echo "<b>Akun Login:</b> <br>";
-echo $email. "<br>";
-echo $password. "<br>";
+  $user = $_GET['user'];
+  $email = $_GET['email'];
+  $pass = $_GET['password'];
+  echo "<b> Akun Login:</b> <br>";
+  echo $user. "<br>";
+  echo $email. "<br>";
+  echo $password. "<br>";
 }
 
 $servername = "localhost";
@@ -22,15 +24,8 @@ if ($conn->connect_error) {
 echo "Connected successfully";
 
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "INSERT INTO account (email, password) 
-VALUES ('novi@mail.com, 'novi')";
+// Insert the data into the database
+$sql = "INSERT INTO customer_acc (id, user, pass, email) VALUES ('$user', '$pass', '$email')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
@@ -39,6 +34,3 @@ if ($conn->query($sql) === TRUE) {
 }
 
 $conn->close();
-?>
-
-
