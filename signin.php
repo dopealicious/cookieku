ea399befe2215ef3901e95e69881ede500da278e
Novitaazahraa <novitaazahra404@gmail.com>
2023-05-24 09:50:32 +0800

    menyambungkan ke database

--- signin.php
+++ signin.php
@@ -1,30 +1,44 @@
 <?php
-// Retrieve the form data
+if (isset($_GET['Input'])) {
+$email = $_GET['email'];
+$password = $_GET['password'];
+echo "<b>Akun Login:</b> <br>";
+echo $email. "<br>";
+echo $password. "<br>";
+}
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "cookieku";
 
-$user= $_POST['name'];
-$pass = $_POST['pass'];
-$email = $_POST['email'];
-
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
-// Insert the data into the database
-$sql = "INSERT INTO account (user, pass, email) VALUES ('$name', '$pass', '$email')";
+echo "Connected successfully";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO account (email, password) 
+VALUES ('novi@gmail.com', 'novi123')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Data inserted successfully";
+  echo "New record created successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// Close the database connection
 $conn->close();
 ?>
+
+
