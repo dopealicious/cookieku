80dbcaa1fc0d698a60c27f32bc4125b53e2e4056
Novitaazahraa <novitaazahra404@gmail.com>
2023-06-02 18:19:38 +0800

    membuat tampilan admin

--- admin/script.js
+++ admin/script.js
@@ -1,22 +1,6 @@
-// Ambil semua tautan navigasi
-var navLinks = document.querySelectorAll("nav ul li a");
+var adminInfo = document.querySelector('.admin-info');
+var dropdownMenu = document.querySelector('.dropdown-menu');
 
-// Tambahkan event listener untuk setiap tautan navigasi
-navLinks.forEach(function (link) {
-  link.addEventListener("click", function (event) {
-    event.preventDefault(); // Mencegah perilaku default tautan
-
-    var target = this.getAttribute("href"); // Ambil target bagian terkait
-
-    // Semua bagian terkait diubah menjadi display "none" secara default
-    var sections = document.querySelectorAll("section");
-    sections.forEach(function (section) {
-      section.style.display = "none";
-    });
-
-    // Tampilkan bagian terkait yang sesuai dengan tautan navigasi yang diklik
-    var targetSection = document.querySelector(target);
-    targetSection.style.display = "block";
-  });
+adminInfo.addEventListener('click', function() {
+  dropdownMenu.classList.toggle('show');
 });
-
